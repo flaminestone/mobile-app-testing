8b99f0717ff8df6e54951425c7a21eaa99b2225c
Dmitriy Rotatyy <flamine@list.ru>
2016-11-04 17:45:38 +0700

    add comments

--- libs/app_manager.rb
+++ libs/app_manager.rb
@@ -4,6 +4,7 @@ require_relative '../libs/helpers/device/device'
 require_relative '../libs/helpers/device/device_helper'
 require 'json'
 class AppManager
+  # method will connect to all devices from configure.json and return array of devices [Device] objects
   def self.initial_devices_by_config(configure = 'configure.json')
     str = File.open(configure, &:read)
     devices = DeviceHelper.create_devices_by_config(JSON.parse(str))
@@ -12,6 +13,7 @@ class AppManager
     end
   end
 
+  # disconnect from all devices
   def self.disconnect_all
     AdbHelper.disconnect_all
   end
