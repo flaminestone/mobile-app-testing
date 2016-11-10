d662b704064dc1ee763e6b6162c59b1bece91d8a
Dmitriy Rotatyy <flamine@list.ru>
2016-11-10 16:39:22 +0700

    add app package name to hash

--- libs/helpers/device/app_requests.rb
+++ libs/helpers/device/app_requests.rb
@@ -1,6 +1,6 @@
 
 class AppRequests
-  APP = {:google_play => {:command => 'com.android.vending/com.google.android.finsky.activities.MainActivity'},
-         :app_install => {:command => 'android.intent.action.VIEW -d'},
-         :onlyoffice => {:command => 'com.onlyoffice.documents/.activities.MainActivity'}}
+  APP = {:google_play => {:command => 'com.android.vending/com.google.android.finsky.activities.MainActivity', :package => 'none'},
+         :app_install => {:command => 'android.intent.action.VIEW -d', :package => 'none'},
+         :onlyoffice => {:command => 'com.onlyoffice.documents/.activities.MainActivity', :package => 'com.onlyoffice.documents'}}
 end
