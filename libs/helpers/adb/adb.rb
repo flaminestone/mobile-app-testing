cbf30571ac6c42ed93db65c9283e73dcee894d4a
Dmitriy Rotatyy <flamine@list.ru>
2016-11-11 17:35:33 +0700

    add opportunity to use scrin slide(swipe)

--- libs/helpers/adb/adb.rb
+++ libs/helpers/adb/adb.rb
@@ -31,6 +31,9 @@ class Adb
     end
 
     def shell_start_by_ip(ip, command, key = 'a')
+      if command == 'com.onlyoffice.documents/.activities.MainActivity'
+          command = '"com.onlyoffice.documents/com.onlyoffice.documents.activities.MainActivity" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER'
+      end
       `adb -s #{ip} shell am start -#{key} #{command} 2<&1`
     end
 
@@ -50,12 +53,16 @@ class Adb
       `adb -s #{ip} shell input text #{text}`
     end
 
-    def hide_keybord(ip)
+    def hide_keyboard(ip)
       `adb -s #{ip} shell input keyevent 111`
     end
 
     def delete_app_data(ip, package_name)
       `adb -s #{ip} shell pm clear #{package_name}`
+    end
+
+    def screen_swipe(ip, x1, y1, x2, y2)
+      `adb -s #{ip} shell input touchscreen swipe #{x1} #{y1} #{x2} #{y2}`
     end
   end
 end
