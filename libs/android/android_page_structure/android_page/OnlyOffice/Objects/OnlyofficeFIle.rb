e9ab907f109ab17680cc2cc4d8e4beae26751b18
Dmitriy Rotatyy <flamine@list.ru>
2016-11-16 21:08:59 +0700

    upgrade common element

---
+++ libs/android/android_page_structure/android_page/OnlyOffice/Objects/OnlyofficeFIle.rb
@@ -0,0 +1,4 @@
+
+class OnlyofficeFile
+
+end
--- libs/android/android_page_structure/common_parser/common_data/Element.rb
+++ libs/android/android_page_structure/common_parser/common_data/Element.rb
@@ -1,7 +1,8 @@
-
+# is a common element for ALL!!! I use it for names, dates and other
 class Element
   attr_accessor :coordinate, :value
   def initialize(*args)
     @coordinate = args.first[:coordinats]
+    @value = args.first[:value]
   end
 end
