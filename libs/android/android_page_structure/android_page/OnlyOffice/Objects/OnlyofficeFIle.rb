06678aa8ac57c456f2e67c4015f8f65ec311a755
Dmitriy Rotatyy <flamine@list.ru>
2016-11-16 23:37:08 +0700

    add OnlyofficeFile class

--- libs/android/android_page_structure/android_page/OnlyOffice/Objects/OnlyofficeFIle.rb
+++ libs/android/android_page_structure/android_page/OnlyOffice/Objects/OnlyofficeFIle.rb
@@ -1,4 +1,10 @@
-
+# Is a class with all data for file
 class OnlyofficeFile
-
+  attr_accessor :name, :owner, :size, :date
+  def initialize(*args)
+    @name = args.first[:filename]
+    @owner = args.first[:fileowner]
+    @size = args.first[:filesize]
+    @date = args.first[:filedate]
+  end
 end
