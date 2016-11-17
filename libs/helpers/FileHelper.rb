5d8b49146317c22076f6b2f6102b20c65897beda
Dmitriy Rotatyy <flamine@list.ru>
2016-11-17 22:11:45 +0700

    create filehelper class and method for delete all files from portal

--- libs/helpers/FileHelper.rb
+++ libs/helpers/FileHelper.rb
@@ -1,7 +1,13 @@
 class FileHelper
   class << self
     def delete_all_files(ip_obj)
-
+      LoggerHelper.print_to_log 'Try to delete all files from portal'
+      files = ip_obj.files.get_my_docs.body['response']['files']
+      return if files.empty?
+      files.each do |current_file|
+        ip_obj.files.delete_file(current_file['id'])
+        LoggerHelper.print_to_log "Delete file with name: #{current_file['title']}"
+      end
     end
   end
 end
