3f7827ac28ac4c2890bc63894735ce807ef3ee1d
Dmitriy Rotatyy <flamine@list.ru>
2016-11-17 22:08:24 +0700

    create folder for scr path

---
+++ libs/android/android_page_structure/android_page/OnlyOffice/OnlyofficeDocumentPage.rb
@@ -0,0 +1,8 @@
+# class with all onlyoffice main page (login page) elements
+require_relative '../../../../../libs/android/android_page_structure/common_parser/common_data/Coordinates'
+require_relative '../../../../../libs/android/android_page_structure/common_parser/common_data/Element'
+require_relative '../../../../../libs/helpers/adb/adb_helper'
+require_relative '../../../../android/android_page_structure/android_page/OnlyOffice/Objects/OnlyofficeFIle'
+module OnlyofficeMainPage
+
+end
