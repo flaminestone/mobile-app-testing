8832ce9a1aa102cec1039add81fa7f41a3745f8d
Dmitriy Rotatyy <flamine@list.ru>
2016-11-18 12:00:01 +0300

    add unoversity for methods: i want to use it with string arguments

--- libs/android/android_parser.rb
+++ libs/android/android_parser.rb
@@ -3,10 +3,10 @@ require_relative '../../libs/android/android_page_structure/Page'
 module AndroidParser
   class AndroidPageStructure < AndroidPageObject
     attr_accessor :ip, :page, :structure
-    def parse(path, ip)
+    def parse(path, ip = nil)
       raise "File #{path} is not found" unless File.exist?(path)
       @ip = ip
-      @xmlobj = Nokogiri::XML(open("#{path}.xml"))
+      @xmlobj = Nokogiri::XML(open(path))
       set_common_data(@xmlobj)
       @page = Page.new(@xmlobj)
       detect_page(@xmlobj)
@@ -14,7 +14,8 @@ module AndroidParser
     end
   end
 
-  def self.parse(filepath, ip)
-    AndroidPageStructure.new.parse(filepath.path, ip)
+  def self.parse(filepath, ip = nil)
+    filepath = "#{filepath.path}.xml" unless filepath.is_a?(String)
+    AndroidPageStructure.new.parse(filepath, ip)
   end
 end
