f3175146142f3268ec1e59570a98df9eeacfa304
Dmitriy Rotatyy <flamine@list.ru>
2017-02-02 12:48:57 +0300

    add reguest for rn google chrom

--- libs/helpers/device/app_requests.rb
+++ libs/helpers/device/app_requests.rb
@@ -2,7 +2,8 @@
 class AppRequests
   APP = { google_play: { command: 'com.android.vending/com.google.android.finsky.activities.MainActivity', package: 'none' },
           app_install: { command: 'android.intent.action.VIEW -d', package: 'none' },
-          onlyoffice: { command: 'com.onlyoffice.documents/.activities.MainActivity', package: 'com.onlyoffice.documents' } }.freeze
+          onlyoffice: { command: 'com.onlyoffice.documents/.activities.MainActivity', package: 'com.onlyoffice.documents' },
+          google_chrome: {command:'com.android.chrome/com.google.android.apps.chrome.Main'}}.freeze
 
   ONLYOFFICE_PAGES_ACTIVITY = { main_page: 'com.onlyoffice.documents/com.onlyoffice.documents.activities.MainActivity',
                     login_page: 'com.onlyoffice.documents/com.onlyoffice.documents.activities.LoginActivity',
